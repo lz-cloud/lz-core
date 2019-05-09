d483af7a74359fbb741faa37c8573fe2ab59c702
wangkc <wkclz@qq.com>
2019-05-10 01:56:16 +0800

    独立应用和cloud 支持

--- src/main/java/com/wkclz/core/config/AuthInterceptor.java
+++ src/main/java/com/wkclz/core/config/AuthInterceptor.java
@@ -1,10 +1,6 @@
 package com.wkclz.core.config;
 
-import com.wkclz.core.helper.AccessHelper;
-import com.wkclz.core.helper.ApiDomainHelper;
-import com.wkclz.core.helper.AuthHelper;
-import com.wkclz.core.helper.OrgDomainHelper;
-import org.apache.commons.lang3.StringUtils;
+import com.wkclz.core.helper.InterceptorHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -13,76 +9,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Description:
+ * Description: 权限
  * Created: wangkaicun @ 2017-10-18 下午11:41
  */
 @Component
 public class AuthInterceptor extends HandlerInterceptorAdapter {
 
     @Autowired
-    private AuthHelper authHelper;
-    @Autowired
-    private AccessHelper accessHelper;
-    @Autowired
-    private ApiDomainHelper apiDomainHelper;
-    @Autowired
-    private OrgDomainHelper orgDomainHelper;
-
+    private InterceptorHelper interceptorHelper;
     @Override
     public boolean preHandle(HttpServletRequest req, HttpServletResponse rep, Object handler) {
-
-        // API 安全检测
-        boolean apiDomainCheckResult = apiDomainHelper.checkApiDomains(req, rep);
-        if (!apiDomainCheckResult){
-            return false;
-        }
-
-        // Org 安全检测
-        boolean orgDomainCheckResult = orgDomainHelper.checkOrgDomains(req, rep);
-        if (!orgDomainCheckResult){
-            return false;
-        }
-
-        // uri 拦截检测【如果检查通过，无需再检查token】
-        boolean checkAccessUriResult = accessHelper.checkAccessUri(req);
-        if (checkAccessUriResult){
-            // 内存 session 检测【不再需要返回判断】【如果存在，不会重复设置】
-            authHelper.checkUserSession(req);
-            return true;
-        }
-
-        // token 检测
-        boolean userTokenCheckResult = authHelper.checkUserToken(req, rep);
-        if (!userTokenCheckResult){
-            return false;
-        }
-
-        // uri 权限检测 TODO
-        boolean userUriAuthCheckResult = true;
-        if (!userUriAuthCheckResult){
-            return false;
-        }
-
-        // 内存 session 检测【不再需要返回判断】【如果存在，不会重复设置】
-        authHelper.checkUserSession(req);
-
-        return true;
+        return interceptorHelper.preHandle(req, rep);
     }
-
 
     @Override
     public void afterCompletion(HttpServletRequest req, HttpServletResponse rep, Object handler, Exception ex) {
-        String token = req.getParameter("token");
-        if (token == null){
-            return;
-        }
-        if (StringUtils.isBlank(token)){
-            return;
-        }
-        if (!token.startsWith("temp_")){
-            return;
-        }
-        authHelper.invalidateSession(token);
+        interceptorHelper.afterCompletion(req);
     }
 
 }
---
+++ src/main/java/com/wkclz/core/helper/InterceptorHelper.java
@@ -0,0 +1,80 @@
+package com.wkclz.core.helper;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Description: 权限
+ * Created: wangkaicun @ 2017-10-18 下午11:41
+ */
+@Component
+public class InterceptorHelper {
+
+    @Autowired
+    private AuthHelper authHelper;
+    @Autowired
+    private AccessHelper accessHelper;
+    @Autowired
+    private ApiDomainHelper apiDomainHelper;
+    @Autowired
+    private OrgDomainHelper orgDomainHelper;
+
+    public boolean preHandle(HttpServletRequest req, HttpServletResponse rep) {
+
+        // API 安全检测
+        boolean apiDomainCheckResult = apiDomainHelper.checkApiDomains(req, rep);
+        if (!apiDomainCheckResult){
+            return false;
+        }
+
+        // Org 安全检测
+        boolean orgDomainCheckResult = orgDomainHelper.checkOrgDomains(req, rep);
+        if (!orgDomainCheckResult){
+            return false;
+        }
+
+        // uri 拦截检测【如果检查通过，无需再检查token】
+        boolean checkAccessUriResult = accessHelper.checkAccessUri(req);
+        if (checkAccessUriResult){
+            // 内存 session 检测【不再需要返回判断】【如果存在，不会重复设置】
+            authHelper.checkUserSession(req);
+            return true;
+        }
+
+        // token 检测
+        boolean userTokenCheckResult = authHelper.checkUserToken(req, rep);
+        if (!userTokenCheckResult){
+            return false;
+        }
+
+        // uri 权限检测 TODO
+        boolean userUriAuthCheckResult = true;
+        if (!userUriAuthCheckResult){
+            return false;
+        }
+
+        // 内存 session 检测【不再需要返回判断】【如果存在，不会重复设置】
+        authHelper.checkUserSession(req);
+
+        return true;
+    }
+
+    public void afterCompletion(HttpServletRequest req) {
+        String token = req.getParameter("token");
+        if (token == null){
+            return;
+        }
+        if (StringUtils.isBlank(token)){
+            return;
+        }
+        if (!token.startsWith("temp_")){
+            return;
+        }
+        authHelper.invalidateSession(token);
+    }
+
+}
