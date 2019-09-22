f23bf7d55414bf692b2ddab3cba533b05a49feea
wangkc <wkclz@qq.com>
2019-09-22 14:01:27 +0800

    获取时间描述

--- src/main/java/com/wkclz/core/util/DateUtil.java
+++ src/main/java/com/wkclz/core/util/DateUtil.java
@@ -24,6 +24,12 @@ public class DateUtil {
     private static final String SDF_YYYY_MM_DD_HH_MM_SS_SSS = "yyyy-MM-dd HH:mm:ss.SSS";
 
 
+    private final static long DAY = 24 * 60 * 60 * 1000L;
+    private final static long HOUR = 60 * 60 * 1000L;
+    private final static long MIN = 60 * 1000L;
+    private final static long SEC = 1000L;
+
+
     /**
      * yyyy-MM-dd
      *
@@ -184,5 +190,68 @@ public class DateUtil {
         calendar.set(Calendar.SECOND, 0);
         return calendar.getTime();
     }
+
+
+    /**
+     * 计算历史到两套上的时间，转换为直观的文字描述
+     * @param history
+     * @return
+     */
+    public static String getTimeDifference(Date history){
+        return getTimeDifference(history, null);
+    }
+
+    /**
+     * 计算历史到两套上的时间，转换为直观的文字描述
+     * @param history
+     * @param future
+     * @return
+     */
+    public static String getTimeDifference(Date history, Date future){
+        long now = System.currentTimeMillis();
+
+        long his = now;
+        long fut = now;
+
+        if (history != null){
+            his = history.getTime();
+        }
+        if (future != null){
+            fut = future.getTime();
+        }
+
+        long timeLess = fut - his;
+
+        long day = timeLess / DAY;
+        timeLess = timeLess % DAY;
+
+        long hour = timeLess / HOUR;
+        timeLess = timeLess % HOUR;
+
+        long min = timeLess / MIN;
+        timeLess = timeLess % MIN;
+
+        long sec = timeLess / SEC;
+
+        StringBuffer sb = new StringBuffer();
+        if (day > 0){
+            sb.append(day + "天 ");
+        }
+        if (day > 0 || hour > 0){
+            sb.append(hour + "时 ");
+        }
+        if (day > 0 || hour > 0 || min > 0){
+            sb.append(min + "分 ");
+        }
+        if (day > 0 || hour > 0 || min > 0 || sec > 0){
+            sb.append(sec + "秒 ");
+        }
+        if (sb.length() == 0){
+            sb.append("0秒");
+        }
+        return sb.toString();
+    }
+
+
 
 }
