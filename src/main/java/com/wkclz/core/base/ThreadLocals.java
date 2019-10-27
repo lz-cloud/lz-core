fb3a622b0634217727ee65047829e5679275e106
wangkc <wkclz@qq.com>
2019-10-27 21:38:27 +0800

    ThreadLocals key check

--- src/main/java/com/wkclz/core/base/ThreadLocals.java
+++ src/main/java/com/wkclz/core/base/ThreadLocals.java
@@ -1,6 +1,8 @@
 package com.wkclz.core.base;
 
 import com.alibaba.fastjson.JSONObject;
+import com.wkclz.core.exception.BizException;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,10 +14,16 @@ public class ThreadLocals {
     private final static ThreadLocal<Map<String, Object>> THREAD_CONTEXT = new ThreadLocal<>();
 
     public static Object get(String key) {
+        if (StringUtils.isBlank(key)){
+            return null;
+        }
         return getContext().get(key);
     }
 
     public static void set(String key, Object value) {
+        if (StringUtils.isBlank(key)){
+            throw BizException.error("key can not be empty when setting ThreadLocals!");
+        }
         Map<String, Object> context = getContext();
         context.put(key, value);
         if (logger.isDebugEnabled()){
