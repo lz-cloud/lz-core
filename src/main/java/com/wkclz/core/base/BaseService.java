989d1d9585edc8ba6e6e703a76d83dba1a9c9c87
wangkc <wkclz@qq.com>
2020-01-08 23:20:35 +0800

    修正 insert 返回

--- src/main/java/com/wkclz/core/base/BaseService.java
+++ src/main/java/com/wkclz/core/base/BaseService.java
@@ -3,6 +3,7 @@ package com.wkclz.core.base;
 import com.wkclz.core.base.annotation.Desc;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,27 +18,27 @@ public class BaseService<Model extends BaseModel, Mapper extends BaseMapper<Mode
     protected Mapper mapper;
 
     @Desc("统计")
-    public Integer count(Model model){
+    public Integer count(@NotNull Model model){
         return mapper.count(model);
     }
 
     @Desc("用ID查找")
-    public Model get(Long id){
+    public Model get(@NotNull Long id){
         return mapper.getById(id);
     }
 
     @Desc("用 Model 查找")
-    public Model get(Model model){
+    public Model get(@NotNull Model model){
         return mapper.getByModel(model);
     }
 
     @Desc("查询列表，不包含Blobs")
-    public List<Model> list(Model model){
+    public List<Model> list(@NotNull Model model){
         return mapper.list(model);
     }
 
     @Desc("查询列分页，不包含Blobs")
-    public PageData<Model> page(Model model){
+    public PageData<Model> page(@NotNull Model model){
         Integer count = mapper.count(model);
         List<Model> list = null;
         if (count > 0){
@@ -51,50 +52,38 @@ public class BaseService<Model extends BaseModel, Mapper extends BaseMapper<Mode
     }
 
     @Desc("(选择性)插入")
-    public Long insert(Model model){
-        return mapper.insert(model);
+    public Long insert(@NotNull Model model){
+        mapper.insert(model);
+        return model.getId();
     }
 
     @Desc("全量批量插入")
-    public Integer insert(List<Model> models){
+    public Integer insert(@NotNull List<Model> models){
         return mapper.insertBatch(models);
     }
 
     @Desc("更新(带乐观锁)")
-    public Integer updateAll(Model model){
+    public Integer updateAll(@NotNull Model model){
         return mapper.updateAll(model);
     }
 
     @Desc("选择性更新(带乐观锁)")
-    public Integer updateSelective(Model model){
+    public Integer updateSelective(@NotNull Model model){
         return mapper.updateSelective(model);
     }
 
     @Desc("批量更新(不带乐观锁)")
-    public Integer update(List<Model> models){
+    public Integer update(@NotNull List<Model> models){
         return mapper.updateBatch(models);
     }
 
-    @Desc("保存(有ID则乐观锁更新,无则新增)")
-    public Long save(Model model){
-        if (model.getId() == null) {
-            return mapper.insert(model);
-        } else {
-            Integer rt = mapper.updateSelective(model);
-            if (rt == 1){
-                return model.getId();
-            }
-            return 0L;
-        }
-    }
-
     @Desc("删除")
-    public Integer delete(Long id){
+    public Integer delete(@NotNull Long id){
         return mapper.delete(id);
     }
 
     @Desc("批量删除")
-    public Integer delete(List<Long> ids){
+    public Integer delete(@NotNull List<Long> ids){
         return mapper.deleteBatch(ids);
     }
 
--- src/main/java/com/wkclz/core/rest/ErrorHandler.java
+++ src/main/java/com/wkclz/core/rest/ErrorHandler.java
@@ -3,33 +3,30 @@ package com.wkclz.core.rest;
 import com.wkclz.core.base.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController
+// @RestController
 public class ErrorHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(ErrorHandler.class);
 
     @GetMapping(value = "/error")
-    public ResponseEntity<Result> error(HttpServletRequest request) {
-
+    public Result error(HttpServletRequest request) {
         Map<String, Object> map = new HashMap<>();
-        map.put("status_code", request.getAttribute("javax.servlet.error.status_code"));
-        map.put("servlet_name", request.getAttribute("javax.servlet.error.servlet_name"));
-        map.put("request_uri", request.getAttribute("javax.servlet.error.request_uri"));
-        map.put("request_uri", request.getAttribute("javax.servlet.forward.request_uri"));
-
-        logger.error("err: {}", map);
+        Enumeration<String> attributeNames = request.getAttributeNames();
+        while (attributeNames.hasMoreElements()){
+            String element = attributeNames.nextElement();
+            map.put(element, request.getAttribute(element));
+        }
+        logger.error("request error: {}", map);
         Result result = new Result();
         result.setData(map);
         result.setCode(-1);
-        return new ResponseEntity<>(result, HttpStatus.BAD_GATEWAY);
+        return result;
     }
 }
