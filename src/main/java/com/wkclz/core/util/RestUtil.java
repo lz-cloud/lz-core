13204fce764c51c63a57abdae1db4acfdb4da83c
wangkc <wkclz@qq.com>
2020-02-05 12:02:48 +0800

    抽取 JDBC 方法

--- src/main/java/com/wkclz/core/util/RestUtil.java
+++ src/main/java/com/wkclz/core/util/RestUtil.java
@@ -18,7 +18,7 @@ import java.util.stream.Collectors;
 
 public class RestUtil {
 
-    private static final Logger logger = LoggerFactory.getLogger(ControllerUtil.class);
+    private static final Logger logger = LoggerFactory.getLogger(RestUtil.class);
 
     /**
      * 读取 controller
