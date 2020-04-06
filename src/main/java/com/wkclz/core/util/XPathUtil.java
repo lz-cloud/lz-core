88d48cabb14b74a5bc8320112f702f4358b017a8
wkclz <wkclz@qq.com>
2020-04-06 13:44:05 +0800

    xpath 查找

--- src/main/java/com/wkclz/core/util/XPathUtil.java
+++ src/main/java/com/wkclz/core/util/XPathUtil.java
@@ -5,10 +5,19 @@ import com.alibaba.fastjson.JSONObject;
 import com.wkclz.core.exception.BizException;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class XPathUtil {
 
+    /**
+     * xPpath 找 json
+     * @param objectStr
+     * @param xPpath
+     * @return
+     */
     public static String path(String objectStr, String xPpath){
         if (StringUtils.isBlank(objectStr)){
             return null;
@@ -22,6 +31,12 @@ public class XPathUtil {
     }
 
 
+    /**
+     * xPpath 找 json, 递归
+     * @param object
+     * @param xPpath
+     * @return
+     */
     private static Object pathValue(Object object, String xPpath){
         if (object == null || xPpath == null){
             return object;
@@ -80,6 +95,89 @@ public class XPathUtil {
         Object o = jsonObject.get(path);
         return pathValue(o, nexPath);
     }
+
+    /**
+     * 从 JSONObject 内查找 xPath
+     * @param objectStr
+     * @param keyword
+     * @return
+     */
+    public static Map<String, String> xPathDiscovery(String objectStr, String keyword){
+        if (StringUtils.isBlank(objectStr)){
+            return null;
+        }
+        JSONObject jsonObject = JSONObject.parseObject(objectStr);
+        // 整理成 map
+        Map<String, String> json2Map = json2Map(jsonObject, null, null);
+
+        if (keyword == null){
+            return json2Map;
+        }
+
+        json2Map = json2Map.entrySet().stream().filter(entry -> entry.getValue().contains(keyword)).collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
+        // 值匹配
+        return json2Map;
+    }
+
+    /**
+     * 递归还原 xpath
+     * @param object
+     * @param basePath
+     * @param values
+     * @return
+     */
+    public static Map<String, String> json2Map(Object object, String basePath, Map<String, String> values){
+        if (object == null){
+            values.put(basePath, "");
+            return values;
+        }
+        if (values == null){
+            values = new HashMap<>();
+        }
+        if (basePath == null){
+            basePath = "";
+        }
+
+        // 找到 leaf 了, 没有下一级
+        if (!(object instanceof JSONObject)){
+            values.put(basePath, object.toString());
+            return values;
+        }
+
+        JSONObject jsonObject = (JSONObject) object;
+
+        Set<String> keySet = jsonObject.keySet();
+        for (String key : keySet) {
+            String path = basePath + "/" + key;
+
+            // 空，找到尽头
+            Object o = jsonObject.get(key);
+
+            // 有下一级，还是数组
+            if (o != null && (o instanceof JSONArray)){
+                JSONArray jsonArray = (JSONArray) o;
+                for (int i = 0; i < jsonArray.size(); i++) {
+                    json2Map(jsonArray.get(i), path + "[" + i + "]" , values);
+                }
+                continue;
+            }
+
+            // 有下一级，是对象, 或没下一级
+            json2Map(o, path, values);
+        }
+        return values;
+    }
+
+    /*
+    public static void main(String[] args) {
+        String url = "http://127.0.0.1:8061/actuator/configprops";
+        HttpRequest get = HttpUtil.createGet(url);
+        HttpResponse execute = get.execute();
+        String body = execute.body();
+        Map<String, String> xx = xPathDiscovery(body, "mysql");
+       System.out.println(xx);
+    }
+    */
 
 
 
