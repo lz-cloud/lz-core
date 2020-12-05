f15a6181191c385be123f3902c38d92ac10c77db
wkclz <wkclz@qq.com>
2020-12-05 17:21:30 +0800

    下拉菜单

---
+++ src/main/java/com/wkclz/core/pojo/entity/Option.java
@@ -0,0 +1,60 @@
+package com.wkclz.core.pojo.entity;
+
+
+import java.util.List;
+
+/**
+ * 下拉菜单
+ */
+public class Option {
+
+    private Long id;
+
+    private Long pid;
+
+    private String label;
+
+    private Long value;
+
+    private List<Option> children;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPid() {
+        return pid;
+    }
+
+    public void setPid(Long pid) {
+        this.pid = pid;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public Long getValue() {
+        return value;
+    }
+
+    public void setValue(Long value) {
+        this.value = value;
+    }
+
+    public List<Option> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Option> children) {
+        this.children = children;
+    }
+}
